66a82b91910f4ec28b3ecfaa6f975b5e74ca6943
Kyle Ludlow <kerrymurphy@Kerrys-iMac.local>
2016-03-17 09:56:14 -0700

    Added POST and GET functionality

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://kyleludlow:Chiodos1432!@ds023098.mlab.com:23098/mma-node-project');
 
+var Fighter = require('./models/fighter');
+
 
 // call the packages we need
 var express    = require('express');        // call express
@@ -21,6 +23,14 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    //do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
@@ -28,6 +38,47 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
+// on routes that end in /fighters
+//----------------------------------------------------------
+router.route('/fighters')
+
+    // create a fighter (accessed at POST http://localhost:8080/api/fighters
+    .post(function(req, res) {
+
+        var fighter = new Fighter();    // create a new instance of the Fighter model
+        fighter.name = req.body.name;   // set the fighters name (comes from request)
+
+        // save the fighter and check for errors
+        fighter.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Fighter created!' });
+        });
+    })
+
+    // get all the fighters (accessed at GET http://localhost:8080/api/fighters)
+    .get(function(req, res) {
+        Fighter.find(function(err, fighters) {
+            if (err)
+                res.send(err);
+
+            res.json(fighters);
+        });
+    });
+
+// on routes that end in /fighters/:fighter_id
+//----------------------------------------------------------
+router.route('fighters/:fighter_id')
+
+    // get the fighter with that id (accessed at GET http://localhost:8080/api/fighters/:fighter_id)
+    .get(function(req, res) {
+        Fighter.findById(req.params.fighter_id, function(err, fighter) {
+            if (err)
+                res.send(err);
+            res.json(fighter);
+        });
+    });
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
